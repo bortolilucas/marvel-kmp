b0630a337ebbe467c74ef5d55911ba28dbcfb604
bortolilucas <lucasbortolidev@gmail.com>
2024-05-28 14:27:52 -0300

    chore: initial commit

---
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,12 @@
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        Text("Hello World")
+    }
+}

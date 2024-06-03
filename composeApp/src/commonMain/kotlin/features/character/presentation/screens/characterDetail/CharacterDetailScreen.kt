67cff670c1bb039b38a6e2c016497b59ba88e59a
thiagosprestes <thiago.santos.prestes@gmail.com>
2024-06-03 11:57:44 -0300

    chore: create a character screen ui

--- composeApp/src/commonMain/kotlin/features/character/presentation/screens/characterDetail/CharacterDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/features/character/presentation/screens/characterDetail/CharacterDetailScreen.kt
@@ -1,18 +1,11 @@
 package features.character.presentation.screens.characterDetail
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.koinScreenModel
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import features.character.presentation.screens.characterDetail.composables.Default
 
 object CharacterDetailScreen : Screen {
     @Composable
@@ -20,16 +13,6 @@ object CharacterDetailScreen : Screen {
         val model = koinScreenModel<CharacterDetailScreenModel>()
         val navigator = LocalNavigator.currentOrThrow
 
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(40.dp, Alignment.CenterVertically),
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Text("Character Detail Screen")
-
-            Button(onClick = { navigator.pop() }) {
-                Text("Voltar")
-            }
-        }
+        Default(onBack = { navigator.pop() })
     }
 }

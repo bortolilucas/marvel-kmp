3a558b8dbf4431d4e0f3c4bbcfb34ae5b55d452c
bortolilucas <lucasbortolidev@gmail.com>
2024-06-05 22:02:43 -0300

    fix: crash koin initialization

--- composeApp/src/androidMain/kotlin/br/com/marvelkmp/app/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/br/com/marvelkmp/app/MainActivity.kt
@@ -4,18 +4,16 @@ import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import di.initKoin
-import logging.util.initNapier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        initKoin()
-        initNapier()
-
+        
+        enableEdgeToEdge()
+        
         setContent {
             App()
         }

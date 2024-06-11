c18f7f529e6d7cf61baa99f864dd4ecd5b131615
thiagosprestes <thiago.santos.prestes@gmail.com>
2024-06-11 16:55:40 -0300

    remove: ScreenModule.kt

--- home/src/commonMain/kotlin/br/com/marvelkmp/home/navigation/HomeScreenNavigationModule.kt
+++ home/src/commonMain/kotlin/br/com/marvelkmp/home/navigation/HomeScreenNavigationModule.kt
@@ -2,6 +2,7 @@ package br.com.marvelkmp.home.navigation
 
 import br.com.marvelkmp.home.presentation.HomeScreen
 import br.com.marvelkmp.navigation.SharedScreen
+import cafe.adriel.voyager.core.registry.screenModule
 
 val homeScreenNavigationModule = screenModule {
     register<SharedScreen.Home> { HomeScreen }
--- home/src/commonMain/kotlin/br/com/marvelkmp/home/navigation/ScreenModule.kt
+++
@@ -1,7 +0,0 @@
-package br.com.marvelkmp.home.navigation
-
-import cafe.adriel.voyager.core.registry.ScreenRegistry
-
-private typealias ScreenModule = ScreenRegistry.() -> Unit
-
-public fun screenModule(block: ScreenModule): ScreenModule = { block() }

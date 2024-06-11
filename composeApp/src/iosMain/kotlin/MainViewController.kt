57ca49df2a7925559c5613cc6d59fe21de69abba
thiagosprestes <thiago.santos.prestes@gmail.com>
2024-06-11 14:52:40 -0300

    fix: resolve import on MainViewController.kt

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,5 +1,6 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import br.com.marvelkmp.logging.util.initNapier
+import navigation.screenRegistry
 
 fun MainViewController() = ComposeUIViewController(
     configure = {

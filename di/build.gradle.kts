d8afccae0444c3e32bacb8dc6aff121bff79fdce
bortolilucas <lucasbortolidev@gmail.com>
2025-01-01 10:35:36 -0300

    feat: pass missing properties to UIKitView

--- di/build.gradle.kts
+++ di/build.gradle.kts
@@ -37,9 +37,9 @@ kotlin {
 
 android {
     namespace = "br.com.marvelkmp.di"
-    compileSdk = 34
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
     defaultConfig {
-        minSdk = 24
+        minSdk = libs.versions.android.minSdk.get().toInt()
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11

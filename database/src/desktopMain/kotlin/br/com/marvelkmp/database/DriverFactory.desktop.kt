2d28a2afb704ba4495eddc62a630398cb2c5c0c7
bortolilucas <lucasbortolidev@gmail.com>
2025-01-02 09:48:20 -0300

    fix: database crash for desktop

--- database/src/desktopMain/kotlin/br/com/marvelkmp/database/DriverFactory.desktop.kt
+++ database/src/desktopMain/kotlin/br/com/marvelkmp/database/DriverFactory.desktop.kt
@@ -2,9 +2,15 @@ package br.com.marvelkmp.database
 
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import java.util.Properties
 
 actual class DriverFactory {
     actual fun createDriver(): SqlDriver =
-        JdbcSqliteDriver("jdbc:sqlite:$MARVEL_DATABASE_NAME")
+        JdbcSqliteDriver(
+            url = "jdbc:sqlite:$MARVEL_DATABASE_NAME",
+            properties = Properties().apply {
+                put("foreign_keys", "true")
+            }
+        )
             .also { MarvelDatabase.Schema.create(it) }
 }

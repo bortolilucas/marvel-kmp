5b1802dff771a9d6b8cb44b30bb25c3f0ba3a9c4
bortolilucas <lucasbortolidev@gmail.com>
2025-01-02 09:10:36 -0300

    feat: adjust Home for desktop

--- features/home/src/commonMain/kotlin/br/com/marvelkmp/home/presentation/composables/CharacterTypeCircleList.kt
+++ features/home/src/commonMain/kotlin/br/com/marvelkmp/home/presentation/composables/CharacterTypeCircleList.kt
@@ -1,8 +1,10 @@
 package br.com.marvelkmp.home.presentation.composables
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,16 +16,16 @@ fun CharacterTypeCircleList(
     onCirclePress: (CharacterFilter) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Row(
+    LazyRow (
         horizontalArrangement = Arrangement.spacedBy(Theme.spacing.medium),
         verticalAlignment = Alignment.CenterVertically,
+        contentPadding = PaddingValues(horizontal = Theme.spacing.large),
         modifier = modifier.fillMaxWidth()
     ) {
-        CharacterFilter.entries.forEach {
+        items(CharacterFilter.entries) {
             CharacterTypeCircle(
                 type = it,
                 onClick = onCirclePress,
-                modifier = Modifier.weight(0.2f)
             )
         }
     }
